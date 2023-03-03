fdc996986e9b61d52ecc0065c4b386383960336a
Anna McCreery <amccreery@madisoncollege.edu>
2023-03-03 12:05:16 -0600

    installing axios and updating function to const

--- src/App.js
+++ src/App.js
@@ -5,33 +5,32 @@ import { API } from 'aws-amplify'
 import './App.css';
 
 const App = () => {
-      function App() {
-        // Create coins variable and set to empty array
-        const [coins, updateCoins] = useState([]);
+    // Create coins variable and set to empty array
+    const [coins, updateCoins] = useState([]);
 
-        // Define function to all API
-        async function fetchCoins() {
-          const data = await API.get('cryptoapi', '/coins')
-          updateCoins(data.coins)
-        };
+    // Define function to all API
+    const fetchCoins = async() => {
+      const data = await API.get('cryptoapi', '/coins')
+      updateCoins(data.coins)
+    };
 
-        // Call fetchCoins function when component loads
-        useEffect(() => {
-          fetchCoins()
-        }, [])
+    // Call fetchCoins function when component loads
+    useEffect(() => {
+      fetchCoins()
+    }, [])
 
-        return (
-          <div className="App">
-            {
-              coins.map((coin, index) => (
-                <div key={index}>
-                  <h2>{coin.name} - {coin.symbol}</h2>
-                  <h5>${coin.price_usd}</h5>
-                </div>
-              ))
-            }
-          </div>
-        );
-      }
-      
-      export default App
+    return (
+      <div className="App">
+        {
+          coins.map((coin, index) => (
+            <div key={index}>
+              <h2>{coin.name} - {coin.symbol}</h2>
+              <h5>${coin.price_usd}</h5>
+            </div>
+          ))
+        }
+      </div>
+    );
+}
+ 
+export default App
